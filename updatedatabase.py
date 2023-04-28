a59b9ab617900fb23ce62d2a51095d9e63f08d4f
zufinho <116960107+zufinho@users.noreply.github.com>
2023-04-28 20:37:13 -0300

    updater fix

--- updatedatabase.py
+++ updatedatabase.py
@@ -5,10 +5,13 @@ with open("databaseversion.txt","r") as ver:
 verifyversionatgithub= requests.get("https://raw.githubusercontent.com/zufinho/pirate-installer/main/databaseversion.txt")
 if not version != verifyversionatgithub:
     getdatabase=requests.get("https://raw.githubusercontent.com/zufinho/pirate-installer/main/database.json")
+    getlistid=requests.get("https://raw.githubusercontent.com/zufinho/pirate-installer/main/listids.txt")
     with open("database.json","w") as databaseupd:
-        databaseupd.write(getdatabase)
+        databaseupd.write(getdatabase.text)
     with open("databaseversion.txt","w") as versionupd:
-        versionupd.write(verifyversionatgithub)
+        versionupd.write(verifyversionatgithub.text)
+    with open("listids.txt","w") as listid:
+        listid.write(getlistid.text)
     print("updated with succesfull!")
 else:
     print("not update needs")
