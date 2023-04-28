44be256f301721405ba272bf10196ac7b99e7fa6
zufinho <116960107+zufinho@users.noreply.github.com>
2023-04-28 20:29:09 -0300

    databaseupdate

--- updatedatabase.py
+++ updatedatabase.py
@@ -1,7 +1,15 @@
 import requests
+from time import sleep
 with open("databaseversion.txt","r") as ver:
     version = ver.read()
 verifyversionatgithub= requests.get("https://raw.githubusercontent.com/zufinho/pirate-installer/main/databaseversion.txt")
-if verifyversionatgithub != version:
+if not version != verifyversionatgithub:
     getdatabase=requests.get("https://raw.githubusercontent.com/zufinho/pirate-installer/main/database.json")
-    print(getdatabase.text)
+    with open("database.json","w") as databaseupd:
+        databaseupd.write(getdatabase)
+    with open("databaseversion.txt","w") as versionupd:
+        versionupd.write(verifyversionatgithub)
+    print("updated with succesfull!")
+else:
+    print("not update needs")
+sleep(5)
