27cc34ead36e7f8ddc5fe0b331f04b83caa972fd
zufinho <116960107+zufinho@users.noreply.github.com>
2023-04-28 21:22:19 -0300

    updater fix

--- updatedatabase.py
+++ updatedatabase.py
@@ -1,18 +1,12 @@
 import requests
 from time import sleep
-with open("databaseversion.txt","r") as ver:
-    version = ver.read()
 verifyversionatgithub= requests.get("https://raw.githubusercontent.com/zufinho/pirate-installer/main/databaseversion.txt")
-if not version == verifyversionatgithub:
-    getdatabase=requests.get("https://raw.githubusercontent.com/zufinho/pirate-installer/main/database.json")
-    getlistid=requests.get("https://raw.githubusercontent.com/zufinho/pirate-installer/main/listids.txt")
-    with open("database.json","w") as databaseupd:
+getdatabase=requests.get("https://raw.githubusercontent.com/zufinho/pirate-installer/main/database.json")
+getlistid=requests.get("https://raw.githubusercontent.com/zufinho/pirate-installer/main/listids.txt")
+with open("database.json","w") as databaseupd:
         databaseupd.write(getdatabase.text)
-    with open("databaseversion.txt","w") as versionupd:
-        versionupd.write(verifyversionatgithub.text)
-    with open("listids.txt","w") as listid:
-        listid.write(getlistid.text)
-    print("updated with succesfull!")
-else:
-    print("not update needs")
-sleep(5)
+with open("databaseversion.txt","w") as versionupd:
+    versionupd.write(verifyversionatgithub.text)
+with open("listids.txt","w") as listid:
+    listid.write(getlistid.text)
+print("updated with succesfull!")
